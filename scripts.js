b4cf31967a57a6e57dd2355dcf2d0f99081b862f
Nicole M Perez <NicoleMariePA@outlook.es>
2022-02-05 22:19:30 -0400

    players icon

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
-playBoard = document.querySelector(".play-board");
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll("section span"),
+players = document.querySelector(".players");
 
 window.onload = () => {
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute("onClick", "clickedBox(this)")
+        
+    }
     selectXBtn.addEventListener("click",()=>{
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
     })
+    selectOBtn.addEventListener("click",()=>{
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+        players.setAttribute("class", "players active");
+    })
+
 };
+let playerXIcon = "fas fa-times"
+let playerOIcon = "far fa-circles"
+
+function clickedBox(element) {
+
+}
